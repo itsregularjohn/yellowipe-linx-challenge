8428df3154c50df20aeccf2f0be7c6beea93d720
João Pedro Oliveira <pedroalves0030@gmail.com>
2025-08-28 20:16:44 -0300

    chore(client): setup tailwind

---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+        ],
+      },
+      colors: {
+        yellowipe: {
+          50: "#fefdf0",
+          100: "#fefbe8",
+          200: "#fef3c7",
+          300: "#fde68a",
+          400: "#facc15",
+          500: "#eab308",
+          600: "#ca8a04",
+          700: "#a16207",
+          800: "#854d0e",
+          900: "#713f12",
+          950: "#422006",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};

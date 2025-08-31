607b9762bfb329aa7f6e12b720ee550716286d43
João Pedro Oliveira <pedroalves0030@gmail.com>
2025-08-31 11:31:29 -0300

    fix(errors): missing forbidden

--- apps/server/src/modules/core/errors.ts
+++ apps/server/src/modules/core/errors.ts
@@ -34,3 +34,10 @@ export class NotFoundError extends HttpError {
     this.name = "NotFoundError";
   }
 }
+
+export class ForbiddenError extends HttpError {
+  constructor(message: string = "Forbidden") {
+    super(message, 403);
+    this.name = "ForbiddenError";
+  }
+}

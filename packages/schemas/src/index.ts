531d719db1666c21053c4dc10ba6e8d66303dfa9
João Pedro Oliveira <pedroalves0030@gmail.com>
2025-08-31 11:00:49 -0300

    refactor(schemas): file organization

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,21 +1,2 @@
-import z from "zod";
-
-export const schema = z.object({
-  hello: z.string().toUpperCase(),
-});
-
-export type Schema = z.infer<typeof schema>;
-
-// Auth schemas
-export * from './user';
-export * from './login';
-export * from './signup';
-export * from './forgot-password';
-export * from './reset-password';
-export * from './send-verification-email';
-export * from './verify-email';
-export * from './update-email';
-export * from './update-password';
-
-// Uploads schemas
+export * from './auth';
 export * from './uploads';
